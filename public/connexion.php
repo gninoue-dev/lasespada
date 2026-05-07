6d5cf90de6c2f103a1950fc31050bd8ae86eceec
Gninoue <devgninoue@gmail.com>
2026-05-07 18:22:31 +0000

    backend de connexion , dashbord,historique,inscription, ok

--- public/connexion.php
+++ public/connexion.php
@@ -41,6 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="../assets/css/"
 
 </head>
 
