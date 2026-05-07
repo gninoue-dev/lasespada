939f0de494b1447c06733701dba3150b46cc6a2f
grah desire <desirejeanivangrah@gmail.com>
2026-05-07 16:24:30 +0000

    interfaces de la page historique.php terminés

---
+++ public/historiques.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/historique.css">
+    <script src="../assets/js/historique.js"></script>
+</head>
+<body>
+    
+
+<body class="large-screen">
+  <div class="wrap">
+    <div class="btn-toolbar buttons">
+      <div class="btn-group">
+    
+      </div>
+      <div class="btn-group">
+    
+      </div>
+    </div>
+    <div class="table-wrapper">
+      <table class="table-responsive card-list-table">
+        <thead>
+          <tr>
+            <th>DATES</th>
+            <th>TYPES</th>
+            <th>SCORES</th>
+            <th>niveaux de risques</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          <tr>
+            <td data-title="Column #1">01-05-2026</td>
+            <td data-title="Column #2">Accident</td>
+            <td data-title="Column #3">85</td>
+            <td data-title="Column #4">Élevé</td>
+            <td data-title="Column #5">normal</td>
+          </tr>
+
+          <tr>
+            <td data-title="Column #1">02-05-2026</td>
+            <td data-title="Column #2">Incendie</td>
+            <td data-title="Column #3">75</td>
+            <td data-title="Column #4">Moyen</td>
+            <td data-title="Column #5">normal</td>
+          </tr>
+          
+            <tr>
+                <td data-title="Column #1">03-05-2026</td>
+                <td data-title="Column #2">Vol</td>
+                <td data-title="Column #3">65</td>
+                <td data-title="Column #4">Faible</td>
+                <td data-title="Column #5">normal</td>  
+           </tr>
+   
+        </tbody>
+      </table>
+    </div>
+  </div>
+</body>
+</body>
+</html>
