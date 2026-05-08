3347e7ba69d390a739127fa8516489ee500f5d80
grah desire <desirejeanivangrah@gmail.com>
2026-05-08 05:55:42 +0000

    modifications de connexion propre maintenant

--- public/connexion.php
+++ public/connexion.php
@@ -49,13 +49,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <script src="https://cdn.tailwindcss.com" defer></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<style>
-    body { background: #f4f4f5; }
-    a { color: purple; text-decoration: underline; }
-    label { color: #52525b; font-size: 14px; }
-</style>
+
 <body>
 <section class="py-32">
     <div class="container">
