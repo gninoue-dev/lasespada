90bf24d4dfb2e8b042112dacfe2a269c68d265ec
Juniordev <alijuniorsidibe30@gmail.com>
2026-05-08 07:12:46 +0000

    fix: bien poussé

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=fraude_assurance;charset=utf8", "root", "root");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur connexion BDD : " . $e->getMessage());
-}
-?>
